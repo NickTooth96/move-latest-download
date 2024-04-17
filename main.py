674fe800bb5bff4bfcb562295c69b3dab89c42e7
NickTooth96 <nicktoothaker@gmail.com>
2024-04-17 16:46:07 -0500

    ADDED: argsparse parser

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import argparse
 import src.download_path as download_path
 import src.most_recent as most_recent
 import src.relocate as relocate
@@ -16,21 +17,34 @@ mr = most_recent.find(dp)
 
 if "args" in sys.argv:
     print(sys.argv)
-   
 
-if "--find" in sys.argv:
-  print(dp,mr)  
-elif "--all" in sys.argv:
+parser = argparse.ArgumentParser(description="Move most recently downloaded file/files to a specified directory.")
+parser.add_argument("--find", help="Find most recently downloaded file/files.", action="store_true")
+parser.add_argument("--all", help="Move all most recently downloaded file/files to specified directory.", action="store_true")
+parser.add_argument("--one", help="Move only the most recently downloaded file to specified directory.", action="store_true")
+parser.add_argument("--undo", help="Undo last move.", action="store_true")
+parser.add_argument("--history", help="Show history of moved files.", action="store_true")
+parser.add_argument("--redo", help="Redo previous move.", action="store_true")
+parser.add_argument("--stat", help="Show statistics of moved files.", action="store_true")
+parser.add_argument("--version", help="Show version of the program.", action="store_true")
+
+args = parser.parse_args()
+
+## Main
+
+if args.find:
+  print(dp,mr)
+elif args.all:
   relocate.move(mr,dp,os.getcwd())
-elif "--one" in sys.argv:
+elif args.one:
   file = []
   file.append(mr[0])
   relocate.move(file,dp,os.getcwd())
-elif "--undo" in sys.argv:
+elif args.undo:
   relocate.undo_last_move()
-elif "--history" in sys.argv:
+elif args.history:
   save_history.read()
-elif "--redo" in sys.argv:
+elif args.redo:
   if sys.argv[sys.argv.index("--redo") + 1]:
     try:
       index = int(sys.argv[sys.argv.index("--redo") + 1])
@@ -39,9 +53,9 @@ elif "--redo" in sys.argv:
     relocate.redo_previous(index)
   else:
     relocate.redo_previous()
-elif "--stat" in sys.argv:
+elif args.stat:
   save_history.stats()
-elif "--version" in sys.argv:
+elif args.version:
   print(NAME,VERSION)
 else:
-  print(ERROR_msg)
+  print('Error: No arguments provided. Use "--help" for more information.')
